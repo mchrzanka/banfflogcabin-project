9f4b29ba0bdba5afd663bb0c03d863703d09f033
mchrzanka <mayachrzanowska@live.com>
2022-10-02 15:36:14 -0600

    updating

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,13 +1,15 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'mysql',
+    client: "mysql",
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),
-      port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'banff_log_cabin'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', 'admin'),
-      ssl: env.bool('DATABASE_SSL', false),
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 3306),
+      database: env("DATABASE_NAME", "banff_log_cabin"),
+      user: env("DATABASE_USERNAME", "root"),
+      password: env("DATABASE_PASSWORD", "admin"),
+      ssl: env.bool("DATABASE_SSL", false),
     },
   },
 });
+
+//THIS IS MINE. PLEASE CREATE YOUR OWN DATABASE AND INPUT THE INFORMATION HERE.
