a2043bd73f5dbebdec9a5eae12d0b7b49f38cfd0
mchrzanka <mayachrzanowska@live.com>
2022-10-06 10:17:15 -0600

    hooks set up and workin'

---
+++ frontend/src/hooks/useFetch.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+
+//get data from strapi
+const useFetch = (url) => {
+	//for the data that we're going to get back from the fetch request
+	const [data, setData] = useState([]);
+	//if we get an error back from strapi
+	const [error, setError] = useState([]);
+	//when we start to use the hook, it will ititialize the loading state to be true and then once we've finished fetching the data we will make it false.
+	const [loading, setLoading] = useState(true);
+
+	useEffect(
+		() => {
+			const fetchData = async () => {
+				setLoading(true);
+
+				try {
+					//we are going to try to fetch the data from the passed in url (the endpoint). Fetch() is an api.
+					const res = await fetch(url);
+					//this is how we store the data, in a javascript object
+					const json = await res.json();
+
+					setData(json);
+					setLoading(false);
+				} catch (error) {
+					setError(error);
+					setLoading(false);
+				}
+			};
+
+			fetchData();
+		},
+		[],
+		{ url }
+	);
+
+	return { loading, error, data };
+};
+
+export default useFetch;
--- frontend/src/pages/homePage.js
+++ frontend/src/pages/homePage.js
@@ -1,6 +1,27 @@
 //shortcut : rfc tab (creates a react functional component)
 import React from 'react';
+import useFetch from '../hooks/useFetch';
 
 export default function HomePage() {
-	return <div>Welcome Home!</div>;
+	const { loading, error, data } = useFetch(
+		'http://localhost:1337/api/reviews'
+	);
+
+	if (loading) {
+		return <p>Loading...</p>;
+	} else if (error === []) {
+		return <p>Error</p>;
+	}
+
+	return (
+		//since strapi update, the data is nested in attributes. So instead of putting {review.content}, you have to do {review.attributes.content}. There was also some issues with strapi passing data as an object now instead of as an array, so instead of data.map you have to do data.data.map, and make sure in useFetch.js the useState is [].
+		<div>
+			{data.data.map((review) => (
+				<div key={review.id}>
+					<div className='content'>{review.attributes.content}</div>
+					<div className='reviewer'>{review.attributes.reviewer}</div>
+				</div>
+			))}
+		</div>
+	);
 }
