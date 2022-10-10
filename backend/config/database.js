e158cfb02a248e7d7df3a41ffaf492f4ca86da33
mchrzanka <mayachrzanowska@live.com>
2022-10-09 21:01:54 -0600

    removed cache

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
     client: "mysql",
     connection: {
       host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
+      port: env.int("DATABASE_PORT", 1337),
       database: env("DATABASE_NAME", "banfflogcabin"),
       user: env("DATABASE_USERNAME", "maya"),
       password: env("DATABASE_PASSWORD", "1382NUGGET"),
