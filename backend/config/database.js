cc5c7f49bdb0d855b958c2e2b315632932f2212d
mchrzanka <mayachrzanowska@live.com>
2022-10-09 20:37:10 -0600

    fix db connection

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,13 +3,14 @@ module.exports = ({ env }) => ({
     client: "postgres",
     connection: {
       host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 1337),
+      port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "banfflogcabin"),
       user: env("DATABASE_USERNAME", "maya"),
       password: env("DATABASE_PASSWORD", "1382NUGGET"),
-      ssl: {
-        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
-      },
+      // ssl: {
+      //   rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+      // },
+      ssl: env("DATABASE_SSL", false),
     },
     debug: false,
   },
