2b7f987a8072fd95e69c2f49eb53c407cb3abb68
mchrzanka <mayachrzanowska@live.com>
2022-10-18 18:45:55 -0600

    fixed page problem

--- frontend/src/pages/bookingPage.js
+++ frontend/src/pages/bookingPage.js
@@ -1,7 +1,6 @@
 //first booking page, for calendar and confirm price/dates. Will have button that says "Continue Booking", that takes you to bookingPageTwo.
 
 import React, { useState } from 'react';
-import CheckoutForm from '../components/forms/CheckoutForm';
 import StripeContainer from '../components/stripe/StripeContainer';
 
 import CalendarMain from '../components/calendar/Calendar';
