306d2a425638842c452e6d22fce80f3748f5f158
mchrzanka <mayachrzanowska@live.com>
2022-10-18 18:44:21 -0600

    Revert "empty calendar."
    This reverts commit a6af6a766ca8de7e7370dded4cacd8a119169ba0.

--- frontend/src/components/calendar/Calendar.js
+++ frontend/src/components/calendar/Calendar.js
@@ -1,1 +1,35 @@
+//lets us set up the router. Installed with npm install react-router-dom
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+//page import
+import HomePage from '../../pages/homePage';
+import OurCabin from '../../pages/ourCabin';
+import AboutUs from '../../pages/aboutUs';
+import Rates from '../../pages/rates';
+import Contact from '../../pages/contact';
+import BookingPage from '../../pages/bookingPage';
+
+//components import
+import SiteHeader from '../header';
+import Footer from '../footer';
+
+function App() {
+	return (
+		<Router>
+			<div className='App'>
+				<SiteHeader />
+				<Routes>
+					<Route path='/' element={<HomePage />}></Route>
+					<Route path='/ourcabin' element={<OurCabin />}></Route>
+					<Route path='/aboutus' element={<AboutUs />}></Route>
+					<Route path='/rates' element={<Rates />}></Route>
+					<Route path='/contact' element={<Contact />}></Route>
+					<Route path='/booking' element={<BookingPage />}></Route>
+				</Routes>
+				<Footer />
+			</div>
+		</Router>
+	);
+}
+
+export default App;
