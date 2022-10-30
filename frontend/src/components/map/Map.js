481f9cae51ee6dabd6125ebc28c02a2ca284da7b
Kevin Russell <71138132+krussell23@users.noreply.github.com>
2022-10-30 11:39:23 -0600

    map changes to react and api key added

--- frontend/src/components/map/Map.js
+++ frontend/src/components/map/Map.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React from 'react'
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
-export default function Map() {
+const containerStyle = {
+	width: '100',
+	height: '400px'
+};
+
+const center = {
+	lat: 51.16851,
+	lng: -115.56585
+};
+
+function MyComponent() {
 	return (
-		<div>
-			{' '}
-			<iframe
-				src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10006.077652491755!2d-115.56883909756138!3d51.17264890698571!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5370ca39085478cb%3A0x8784453d730a821f!2sBanff%20Log%20Cabin%20Guesthouse!5e0!3m2!1sen!2sca!4v1666039017911!5m2!1sen!2sca'
-				style={{ width: 600, height: 800 }}
-				referrerPolicy='no-referrer-when-downgrade'
-			></iframe>
-		</div>
-	);
+		<LoadScript
+			googleMapsApiKey="AIzaSyDsNJ1kSAhoGnRHAGE9kd1F9MMpatsBNR4"
+		>
+			<GoogleMap
+				mapContainerStyle={containerStyle}
+				center={center}
+				zoom={15}
+			>
+				{ /* Child components, such as markers, info windows, etc. */}
+				<></>
+			</GoogleMap>
+		</LoadScript>
+	)
 }
+
+export default React.memo(MyComponent)
