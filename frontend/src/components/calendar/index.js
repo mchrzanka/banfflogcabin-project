00e6a5443b46c74a7b9acb6c05caffc6da5c8229
Maya <70867990+mchrzanka@users.noreply.github.com>
2022-10-30 10:19:13 -0600

    conflict resolved

---
+++ frontend/src/components/calendar/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Calen from "./Calen";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+    <React.StrictMode>
+        <Calen />
+    </React.StrictMode>,
+    rootElement
+);
