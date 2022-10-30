103a93405da79702d5e920ade698ae7e0839c6d3
Maya <70867990+mchrzanka@users.noreply.github.com>
2022-10-30 10:25:32 -0600

    conflict resolved

--- frontend/src/pages/bookingPage.js
+++ frontend/src/pages/bookingPage.js
@@ -1,6 +1,10 @@
 //first booking page, for calendar and confirm price/dates. Will have button that says "Continue Booking", that takes you to bookingPageTwo.
 
+<<<<<<< Updated upstream
 import React, { useState, useRef } from 'react';
+=======
+import React, { useState } from 'react';
+>>>>>>> Stashed changes
 import StripeContainer from '../components/stripe/StripeContainer';
 
 import Calen from './../components/calendar/Calen';
@@ -12,7 +16,11 @@ export default function BookingPageOne() {
 		<div>
 			<div>Booking Page 1</div>
 
+<<<<<<< Updated upstream
 			<div><Calen /></div>
+=======
+			{/* <div><Calen /></div> */}
+>>>>>>> Stashed changes
 			<div>
 				{showItem ? (
 					<div>
@@ -23,9 +31,13 @@ export default function BookingPageOne() {
 					<>
 						{' '}
 						<h3>$10.00</h3>
+<<<<<<< Updated upstream
 						
 						<button className='tan' onClick={() => {console.log(Calen.data); setShowItem(true);}}>Continue</button>
 						
+=======
+						<button className='tan' onClick={() => setShowItem(true)}>Continue</button>
+>>>>>>> Stashed changes
 					</>
 				)}
 			</div>
