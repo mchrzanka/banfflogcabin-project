1177c24016cc713792af4f37b39c2ed7b4455089
Maya <70867990+mchrzanka@users.noreply.github.com>
2022-10-30 10:18:35 -0600

    conflict resolved

--- frontend/src/components/reviews/reviews.js
+++ frontend/src/components/reviews/reviews.js
@@ -16,6 +16,7 @@ const Reviews = () => {
 
 	return (
 		//I THINK: data? GETS RID OF NO RESPONSE ERROR. since strapi update, the data is nested in attributes. So instead of putting {review.content}, you have to do {review.attributes.content}. There was also some issues with strapi passing data as an object now instead of as an array, so instead of data.map you have to do data.data.map, and make sure in useFetch.js the useState is [].
+		//since strapi update, the data is nested in attributes. So instead of putting {review.content}, you have to do {review.attributes.content}. There was also some issues with strapi passing data as an object now instead of as an array, so instead of data.map you have to do data.data.map, and make sure in useFetch.js the useState is [].
 		<div>
 			{data.data.map((review) => (
 				<div key={review.id}>
--- frontend/src/pages/bookingPage.js
+++ frontend/src/pages/bookingPage.js
@@ -1,6 +1,5 @@
 //first booking page, for calendar and confirm price/dates. Will have button that says "Continue Booking", that takes you to bookingPageTwo.
 
-import React, { useState, useRef } from 'react';
 import StripeContainer from '../components/stripe/StripeContainer';
 
 import Calen from './../components/calendar/Calen';
@@ -12,7 +11,6 @@ export default function BookingPageOne() {
 		<div>
 			<div>Booking Page 1</div>
 
-			<div><Calen /></div>
 			<div>
 				{showItem ? (
 					<div>
@@ -23,9 +21,6 @@ export default function BookingPageOne() {
 					<>
 						{' '}
 						<h3>$10.00</h3>
-						
-						<button className='tan' onClick={() => {console.log(Calen.data); setShowItem(true);}}>Continue</button>
-						
 					</>
 				)}
 			</div>
