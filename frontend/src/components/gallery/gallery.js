562b500c60193cb364ff653cd42462deaaefbeb2
Justin Shaw <shaw.justin@shaw.ca>
2022-11-02 14:35:09 -0600

    Gallery images work, part one
    Part one of making the image gallery and ourcabin page

---
+++ frontend/src/components/gallery/gallery.js
@@ -0,0 +1,28 @@
+import React from 'react';
+// import './styles.css';
+import '../../scss/components/_gallery.scss';
+
+import breakfast from '../../img/galleryImages/breakfast-outside.jpg';
+import bedroomone from '../../img/galleryImages/bedroom-1.jpg';
+import kitchen from '../../img/galleryImages/kitchen.jpg';
+import bathroom from '../../img/galleryImages/bathroom.jpg';
+import customerbook from '../../img/galleryImages/customer-book.jpg';
+import bedroomtwo from '../../img/galleryImages/bedroom-2.jpg';
+import waffles from '../../img/galleryImages/waffles.jpg';
+import backdoor from '../../img/galleryImages/cabin-backdoor.jpg';
+
+
+export default function Gallery() {
+    return <div className='gallery'>
+        <div className='images'>
+            <img src={breakfast} />
+            <img src={bedroomone} />
+            <img src={kitchen} />
+            <img src={bathroom} />
+            <img src={customerbook} />
+            <img src={bedroomtwo} />
+            <img src={waffles} />
+            <img src={backdoor} />
+        </div>
+    </div>
+}
--- frontend/src/pages/ourCabin.js
+++ frontend/src/pages/ourCabin.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import useFetch from '../hooks/useFetch';
 import Play from '../components/youtube/play';
+import Gallery from '../components/gallery/gallery';
+
+import '../scss/pages/_ourcabin.scss'
 
 export default function OurCabin() {
 	const { loading, error, data } = useFetch(
@@ -22,11 +25,13 @@ export default function OurCabin() {
 				<div className='container'>
 					<div className='content'>{data.data.attributes.content1}</div>
 				</div>
-				<div className='container'>
-					<div className='quote'>{data.data.attributes.quote}</div>
+				<Play />
+				<div className='gallery-container'>
+					<div className='quote container'>{data.data.attributes.quote}</div>
+					<Gallery />
 				</div>
 			</div>
-			<Play />
+
 		</div>
 	);
 }
