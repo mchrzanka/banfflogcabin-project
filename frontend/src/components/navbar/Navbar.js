775e15f8a77b1cac222f69789f38a00f9131181f
mchrzanka <mayachrzanowska@live.com>
2022-11-10 12:27:42 -0700

    navbar fixed

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -11,54 +11,52 @@ const Navbar = () => {
 	return (
 		<>
 			<header>
-				<nav>
-					<div className='main-menu container'>
-						<div className='logo'>
-							<a href='/'>
-								<img
-									width='40'
-									height='40'
-									alt='Banff Log Cabin Logo'
-									src='https://miro.medium.com/max/1400/0*0fClPmIScV5pTLoE.jpg'
-								/>
-							</a>
-						</div>
-						<div className={isActive ? 'mobile-menu is-active' : 'mobile-menu'}>
-							<ul>
-								<li className='navitem'>
-									<a href='/'>Home</a>
-								</li>
-								<li className='navitem'>
-									<a href='/ourcabin'>Our Cabin</a>
-								</li>
-								<li className='navitem'>
-									<a href='/aboutus'>About us</a>
-								</li>
-								<li className='navitem'>
-									<a href='/rates'>Rates</a>
-								</li>
-								<li className='navitem'>
-									<a href='/contact'>Contact</a>
-								</li>
-								<li className='booknow-nav'>
-									<a href='/bookingterms'>Book Now</a>
-								</li>
-							</ul>
-						</div>
-
-						<div
-							onClick={handleClick}
-							aria-label='Main Menu'
-							className={isActive ? 'hamburger is-active' : 'hamburger'}
-						>
-							<span className='roofline'></span>
-							<span className='roofline2'></span>
-							<span className='line'></span>
-							<span className='line'></span>
-							<span className='line'></span>
-						</div>
+				<div className='navContainer'>
+					<div className='logo'>
+						<a href='/'>
+							<img
+								width='40'
+								height='40'
+								alt='Banff Log Cabin Logo'
+								src='https://miro.medium.com/max/1400/0*0fClPmIScV5pTLoE.jpg'
+							/>
+						</a>
 					</div>
-				</nav>
+					<div className={isActive ? 'mobile-menu is-active' : 'mobile-menu'}>
+						<ul>
+							<li className='navitem'>
+								<a href='/'>Home</a>
+							</li>
+							<li className='navitem'>
+								<a href='/ourcabin'>Our Cabin</a>
+							</li>
+							<li className='navitem'>
+								<a href='/aboutus'>About us</a>
+							</li>
+							<li className='navitem'>
+								<a href='/rates'>Rates</a>
+							</li>
+							<li className='navitem'>
+								<a href='/contact'>Contact</a>
+							</li>
+							<li className='booknow-nav'>
+								<a href='/bookingterms'>Book Now</a>
+							</li>
+						</ul>
+					</div>
+
+					<div
+						onClick={handleClick}
+						aria-label='Main Menu'
+						className={isActive ? 'hamburger hammy-is-active' : 'hamburger'}
+					>
+						<span className='roofline'></span>
+						<span className='roofline2'></span>
+						<span className='line'></span>
+						<span className='line'></span>
+						<span className='line'></span>
+					</div>
+				</div>
 			</header>
 		</>
 	);
