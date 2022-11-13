0c21e4543f490aafc03efe2be6b8c0d3227b1bf8
Maya <70867990+mchrzanka@users.noreply.github.com>
2022-11-13 16:54:28 -0700

    date format on rates page

--- frontend/src/components/pricing/Pricing.js
+++ frontend/src/components/pricing/Pricing.js
@@ -1,8 +1,8 @@
 import React from "react";
 import useFetch from "../../hooks/useFetch";
+import Moment from 'moment';
 
-
-import '../../scss/components/_pricing.scss'
+import '../../scss/components/_pricing.scss';
 
 const Pricing = () => {
   const { loading, error, data } = useFetch(
@@ -15,6 +15,30 @@ const Pricing = () => {
     return <p>Error</p>;
   }
 
+  let price = [data.data];
+  price = price[0];
+
+  let seasonInfo = {};
+  let season;
+
+  for (let i = 0; i < price.length; i++) {
+    const seasonalPricing = price[i].attributes;
+    season = seasonalPricing.season;
+
+    let startDateOfSeason = seasonalPricing.startdate;
+    let endDateOfSeason = seasonalPricing.enddate;
+
+    startDateOfSeason = new Date(startDateOfSeason);
+    endDateOfSeason = new Date(endDateOfSeason);
+
+    seasonInfo = {
+      Start: startDateOfSeason.toISOString(),
+      End: endDateOfSeason.toISOString(),
+    };
+
+console.log(Moment(seasonInfo.Start).format('MMM Do'));
+  }
+
   return (
     //since strapi update, the data is nested in attributes. So instead of putting {review.content}, you have to do {review.attributes.content}. There was also some issues with strapi passing data as an object now instead of as an array, so instead of data.map you have to do data.data.map, and make sure in useFetch.js the useState is [].
     <div className="pricing">
@@ -22,7 +46,7 @@ const Pricing = () => {
         <div key={pricing.id} className='block'>
           <p>{pricing.attributes.season}</p>
           <p>
-            {pricing.attributes.startdate} - {pricing.attributes.enddate}
+          {Moment(seasonInfo.Start).format('MMM Do')} - {Moment(seasonInfo.End).format('MMM Do')}
           </p>
           <p>${pricing.attributes.price}/night</p>
         </div>
