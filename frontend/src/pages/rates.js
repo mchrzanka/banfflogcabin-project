66b9f4d3a8d0fd3f5c4a178d9758b93cc10d5834
Justins45 <shaw.justin@shaw.ca>
2022-11-13 15:24:09 -0700

    fixed homepage banner
    ******* *** *** *** *** * ** * ** **** ** AHSFGAIPFGA
    
    added width: fit-content:
    Because it would center

--- frontend/src/pages/rates.js
+++ frontend/src/pages/rates.js
@@ -3,6 +3,8 @@ import Ammenities from '../components/ammenities/Ammenities';
 import useFetch from '../hooks/useFetch';
 import Pricing from './../components/pricing/Pricing';
 
+
+
 export default function Rates() {
 	const { loading, error, data } = useFetch(
 		'http://147.182.207.198:1337/api/ratespage'
@@ -21,7 +23,7 @@ export default function Rates() {
 			<Pricing />
 			<p className='content'>{data.data.attributes.content1}</p>
 			<h2 className='heading2'>{data.data.attributes.h2}</h2>
-			<Ammenities/>
+			<Ammenities />
 			<p className='content'>{data.data.attributes.content2}</p>
 		</div>
 	);
