98c43bce2b0cd0b261d36cc6d57f29c623c17c24
Kevin Russell <71138132+krussell23@users.noreply.github.com>
2022-11-14 10:06:34 -0700

    success working but overlaying image everywhere

--- frontend/src/pages/success.js
+++ frontend/src/pages/success.js
@@ -1,16 +1,45 @@
 import React from 'react';
 
+import '../scss/pages/_success.scss'
+
+
+
+
+
+
+
 export default function SuccessPage() {
     return (
-        <div>
+
+
+        <div class="container">
 
             <h1>Booking Confirmed.</h1>
-            <h1>See you soon!</h1>
+            <h2>See you soon!</h2>
             <p>Your booking has been successfully confirmed. A confirmation email will be sent shortly.</p>
             <p>If you need to make changes to your booking, please email us at <a href="mailto: reservations@banfflogcabin.ca">reservations@banfflogcabin.ca</a></p>
+
+            <div class="bird-container bird-container--one">
+                <div class="bird bird--one"></div>
+            </div>
+
+            <div class="bird-container bird-container--two">
+                <div class="bird bird--two"></div>
+            </div>
+
+            <div class="bird-container bird-container--three">
+                <div class="bird bird--three"></div>
+            </div>
+
+            <div class="bird-container bird-container--four">
+                <div class="bird bird--four"></div>
+            </div>
+
         </div>
 
-    );
 
+
+    );
 };
+
 
