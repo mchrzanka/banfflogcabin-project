240766efbaf516820202520489a5ae82b9a46d5f
Kevin Russell <71138132+krussell23@users.noreply.github.com>
2022-11-14 10:26:52 -0700

    working now

--- frontend/src/pages/success.js
+++ frontend/src/pages/success.js
@@ -12,13 +12,12 @@ export default function SuccessPage() {
     return (
 
 
-        <div class="container">
-
-            <h1>Booking Confirmed.</h1>
-            <h2>See you soon!</h2>
-            <p>Your booking has been successfully confirmed. A confirmation email will be sent shortly.</p>
-            <p>If you need to make changes to your booking, please email us at <a href="mailto: reservations@banfflogcabin.ca">reservations@banfflogcabin.ca</a></p>
-
+        <div class="container-anim">
+            <div class="message-thanks">
+                <h1>Booking Confirmed.</h1>
+                <h2>See you soon!</h2>
+                <p>If you need to make changes to your booking, please email us at <a href="mailto: reservations@banfflogcabin.ca">reservations@banfflogcabin.ca</a></p>
+            </div>
             <div class="bird-container bird-container--one">
                 <div class="bird bird--one"></div>
             </div>
