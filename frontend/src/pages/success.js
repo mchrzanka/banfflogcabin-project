31d1fa817ea667c40c0045f99883fa35b958485f
Maya <70867990+mchrzanka@users.noreply.github.com>
2022-11-13 18:47:24 -0700

    success page wording

--- frontend/src/pages/success.js
+++ frontend/src/pages/success.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 export default function SuccessPage() {
     return (
-        <div className="Thankyou">
+        <div>
 
-            <h1 className='successh1'>Thank you from Banff Log Cabin</h1>
-            <p className="successp">wohoo....Your reservation is confirmed</p>
+            <h1>Booking Confirmed.</h1>
+            <h1>See you soon!</h1>
+            <p>Your booking has been successfully confirmed. A confirmation email will be sent shortly.</p>
+            <p>If you need to make changes to your booking, please email us at <a href="mailto: reservations@banfflogcabin.ca">reservations@banfflogcabin.ca</a></p>
         </div>
 
     );
