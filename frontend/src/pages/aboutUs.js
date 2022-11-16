4c9834880fd1f97f0acaa35b95a4777aacda7632
Maya C <70867990+mchrzanka@users.noreply.github.com>
2022-11-16 14:07:36 -0700

    about us fixes

--- frontend/src/pages/aboutUs.js
+++ frontend/src/pages/aboutUs.js
@@ -29,17 +29,15 @@ export default function AboutUs() {
 				</div>
 				<div className='heading1'>{data.data.attributes.h1}</div>
 				<div className="flex-con">
-					<div className='heading-con'>
+					<div>
 						<div className='heading2'>{data.data.attributes.h2}</div>
 						<div className='content'>{data.data.attributes.content1}</div>						
-					</div>
-					<div className='large-con'>
-						<div className='larger-text'>{data.data.attributes.quote}</div>
-					</div>
-					<div className='last-con'>
 						<div className='content'>{data.data.attributes.content2}</div>					
 						<div className='content'>{data.data.attributes.content3}</div>
 						<div className='content'>{data.data.attributes.content4}</div>
+						</div>
+					<div className='quote'>
+						<div>{data.data.attributes.quote}</div>
 					</div>
 				
 				</div>
