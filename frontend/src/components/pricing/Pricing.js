41769aa12ea95a455af77a7b83bd18486f453b0d
mchrzanka <mayachrzanowska@live.com>
2022-11-17 16:35:01 -0700

    extra on rates fixed

--- frontend/src/components/pricing/Pricing.js
+++ frontend/src/components/pricing/Pricing.js
@@ -1,38 +1,41 @@
-import React from "react";
-import useFetch from "../../hooks/useFetch";
+import React from 'react';
+import useFetch from '../../hooks/useFetch';
 import Moment from 'moment';
 
 import '../../scss/components/_pricing.scss';
 
 const Pricing = () => {
-  const { loading, error, data } = useFetch(
-    "http://147.182.207.198:1337/api/pricings"
-  );
+	const { loading, error, data } = useFetch(
+		'http://147.182.207.198:1337/api/pricings'
+	);
 
-  if (loading) {
-    return <p>Loading...</p>;
-  } else if (error === []) {
-    return <p>Error</p>;
-  }
+	if (loading) {
+		return <p>Loading...</p>;
+	} else if (error === []) {
+		return <p>Error</p>;
+	}
 
-  return (
-    <div className="pricing">
-      {data.data.map((pricing) => (
-        <div key={pricing.id} className='block'>
-          <p>{pricing.attributes.season}</p>
-          <p>{Moment(pricing.attributes.startdate).format('MMM Do')} - {Moment(pricing.attributes.enddate).format('MMM Do')}</p>
-          <p>${pricing.attributes.price}/night</p>
-        </div>
-      ))}
+	return (
+		<div className='pricing'>
+			{data.data.map((pricing) => (
+				<div key={pricing.id} className='block'>
+					<p>{pricing.attributes.season}</p>
+					<p>
+						{Moment(pricing.attributes.startdate).format('MMM Do')} -{' '}
+						{Moment(pricing.attributes.enddate).format('MMM Do')}
+					</p>
+					<p>${pricing.attributes.price}/night</p>
+				</div>
+			))}
 
-      <div className="block extra">
-        <p>Please Note:</p>
-        <p className="ex-p">All prices will include an additional 5% GST.</p>
-        <p className="ex-p">Minimim booking of two nights required.</p>
-        <p className="ex-p">A deposit will be required at time of booking.</p>
-      </div>
-    </div>
-  );
+			<div className='extra'>
+				<p>Please Note:</p>
+				<p className='ex-p'>All prices will include an additional 5% GST.</p>
+				<p className='ex-p'>Minimim booking of two nights required.</p>
+				<p className='ex-p'>A deposit will be required at time of booking.</p>
+			</div>
+		</div>
+	);
 };
 
 export default Pricing;
