e56145b40825fa4813012aa5057d1e2dab2a9c39
Kevin Russell <71138132+krussell23@users.noreply.github.com>
2022-11-24 08:20:22 -0700

    pmnt fail and image embed for success

---
+++ frontend/src/pages/pmntfail.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../scss/pages/_pmntfail.scss';
+
+export default function PmntFail() {
+    return (
+        <div className='container-pmnt'>
+            <div className='message-thanks'>
+                <h1>Sorry Your Payment Did Not Process</h1>
+                <h2>Please Try Another Medhod</h2>
+
+            </div>
+
+        </div>
+    );
+}
