3775ce6adbc36ec4b5698085d520c147d568588a
Maya <70867990+mchrzanka@users.noreply.github.com>
2022-11-24 19:51:31 -0700

    Revert "stuff"
    This reverts commit 3c01fc75e7d3cf71a2c00ca5a990a3f2f9aabb79.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,12 +9,12 @@ import Rates from './pages/rates';
 import Contact from './pages/contact';
 import BookingPage from './pages/bookingPage';
 import BookingTerms from './pages/bookingTerms';
-import Success from './pages/success';
+import SuccessPage from './pages/success';
+import PmntFail from './pages/pmntfail';
 
 //components import
 import SiteHeader from './components/header/header';
 import Footer from './components/footer/footer';
-import SuccessPage from './pages/success';
 
 
 function App() {
--- frontend/src/pages/pmntfail.js
+++ frontend/src/pages/pmntfail.js
@@ -6,7 +6,7 @@ export default function PmntFail() {
         <div className='container-pmnt'>
             <div className='message-thanks'>
                 <h1>PAYMENT FAILED</h1>
-                <h2>Your Transaction has failed due to an unknown error.</h2>
+                <h2>Your Transaction has failed due to unknown error.</h2>
                 <h2>Please try again!</h2>
 
             </div>
