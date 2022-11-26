add13a69c23625e7cdfc5f38db3841ef925eedf4
Maya <70867990+mchrzanka@users.noreply.github.com>
2022-11-26 11:21:37 -0700

    padding and wording fix

--- frontend/src/pages/paymentfailed.js
+++ frontend/src/pages/paymentfailed.js
@@ -5,10 +5,10 @@ export default function PmntFail() {
     return (
         <div className='container-pmnt'>
             <div className='message-pmnt'>
-                <h1>PAYMENT FAILED</h1>
+                <h1>Payment Error</h1>
                 <p>Your Transaction has failed due to an unknown error.</p>
                 <p>Please try again!</p>
-                <a className="primary-btn btn " href="/bookingterms">
+                <a className="primary-btn btn " href="/booking">
                     Back to Booking
                 </a>
             </div>
