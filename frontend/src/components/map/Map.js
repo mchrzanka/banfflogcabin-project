d9718ec3c725b449452413a75dc577cb2c4c7426
mchrzanka <mayachrzanowska@live.com>
2023-01-25 10:56:54 -0700

    commit
    commit

--- frontend/src/components/map/Map.js
+++ frontend/src/components/map/Map.js
@@ -1,30 +1,24 @@
-import React from 'react'
+import React from 'react';
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
 const containerStyle = {
-	width: '100'
+	width: '100',
 };
 
 const center = {
 	lat: 51.16851,
-	lng: -115.56585
+	lng: -115.56585,
 };
 
 function MyComponent() {
 	return (
-		<LoadScript
-			googleMapsApiKey="AIzaSyDsNJ1kSAhoGnRHAGE9kd1F9MMpatsBNR4"
-		>
-			<GoogleMap
-				mapContainerStyle={containerStyle}
-				center={center}
-				zoom={15}
-			>
-				{ /* Child components, such as markers, info windows, etc. */}
+		<LoadScript googleMapsApiKey='process.env.REACT_APP_GOOGLE_MAPS'>
+			<GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
+				{/* Child components, such as markers, info windows, etc. */}
 				<></>
 			</GoogleMap>
 		</LoadScript>
-	)
+	);
 }
 
-export default React.memo(MyComponent)
+export default React.memo(MyComponent);
