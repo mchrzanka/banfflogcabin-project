dc028579f553b6e9e5f31e5b49005f0802942a43
mchrzanka <mayachrzanowska@live.com>
2023-02-15 10:47:03 -0700

    mobile contact page

--- frontend/src/pages/contact.js
+++ frontend/src/pages/contact.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ContactForm from '../components/forms/ContactForm';
 import useFetch from '../hooks/useFetch';
-import Map from '../img/map.PNG';
+import Map from '../img/map-small.PNG';
 
 import '../scss/pages/_contact.scss';
 export default function Contact() {
@@ -30,20 +30,16 @@ export default function Contact() {
 			</div>
 			<div className='find-map'>
 				<div className='find-us'>
-					<div className='find-pad'>
-						<div className='h2'>{data.data.attributes.h2}</div>
-						<div className='content'>{data.data.attributes.content2}</div>
-					</div>
+					<div className='h2'>{data.data.attributes.h2}</div>
+					<div className='content'>{data.data.attributes.content2}</div>
 				</div>
-				<div className='map'>
-					<a
-						href='https://www.google.com/maps/place/Banff+Log+Cabin+Guesthouse/@51.1683881,-115.5680137,17z/data=!3m1!4b1!4m9!3m8!1s0x5370ca39085478cb:0x8784453d730a821f!5m2!4m1!1i2!8m2!3d51.1683848!4d-115.565825!16s%2Fg%2F11f3f9y6vn'
-						target='_blank'
-						rel='noreferrer'
-					>
-						<img src={Map} />
-					</a>
-				</div>
+				<a
+					href='https://www.google.com/maps/place/Banff+Log+Cabin+Guesthouse/@51.1683881,-115.5680137,17z/data=!3m1!4b1!4m9!3m8!1s0x5370ca39085478cb:0x8784453d730a821f!5m2!4m1!1i2!8m2!3d51.1683848!4d-115.565825!16s%2Fg%2F11f3f9y6vn'
+					target='_blank'
+					rel='noreferrer'
+				>
+					<div className='map-bg'></div>
+				</a>
 			</div>
 		</div>
 	);
